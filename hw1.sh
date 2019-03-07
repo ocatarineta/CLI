ae6de95affeb4d8e55c8592904b4f623e19ad6db
ocatarineta <47867362+ocatarineta@users.noreply.github.com>
2019-03-07 09:29:56 -0800

    First week attempt
    First week attempt

---
+++ hw1.sh
@@ -0,0 +1,29 @@
+mkdir ./hw1.sh
+cd ahmed
+touch blank.txt
+
+echo "Hello" >> greetings.txt
+
+for index in 1 2 3 4 5
+do
+ cp greetings.txt $index.txt
+ done 
+ for e 1 2 3 4 5 
+ do
+ cp greetings.txt $e.txt
+ done
+
+ echo "cat" >> pets.txt
+ echo "dog" >> pets.txt
+ echo "hamster" >> pets.txt
+
+ echo "cat" >> commands.txt
+ echo "ls" >> commands.txt
+ echo "pwd" >> commands.txt
+
+ cat pets.txt commands.txt 
+cp greetings.txt.1.txt
+cp greetings.txt.2.txt
+cp greetings.txt.3.txt
+cp greetings.txt.4.txt
+cp greetings.txt.5.txt
